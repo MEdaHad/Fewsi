a0ef7c362761e6cc29600ac17f6173f6e90ccaa5
MEdaHad <medahad16@gmail.com>
2023-01-08 22:10:31 +0100

    initial Commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express  from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+ 

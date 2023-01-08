9120474ed00040c04e3bc8912578ec90626ddb51
MEdaHad <medahad16@gmail.com>
2023-01-08 22:29:50 +0100

    initial

--- server/index.js
+++ server/index.js
@@ -7,3 +7,10 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
  
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+DeviceMotionEvent.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePlicy({ policy: "cross-origin"}));

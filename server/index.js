a155e574c7cd877802c5be963ebb8c509f745cd9
MEdaHad <medahad16@gmail.com>
2023-01-15 20:24:02 +0100

    MongoDBenvSetUp

--- server/index.js
+++ server/index.js
@@ -27,7 +27,11 @@ const storage = multer.diskStorage({
      filename: function(req, file, cb){
         cb(null, file.originalname)
      }
-})
+}); 
+
+const upload = multer ({ storage });
+ 
+
 
 
 

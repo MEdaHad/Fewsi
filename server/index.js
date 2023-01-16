64c463eb36873490dea30ab75a2080e5021991e0
MEdaHad <medahad16@gmail.com>
2023-01-16 02:03:50 +0100

    fewUpd

--- server/index.js
+++ server/index.js
@@ -2,23 +2,25 @@ import express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import multer from "multer";
+import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
  
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-DeviceMotionEvent.config();
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePlicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.json(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 const storage = multer.diskStorage({
      destination:function(req, file, cb) {
@@ -36,9 +38,10 @@ const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
-}).then(()=> {
+})
+.then(()=> {
    app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`));
-}).catch((erorr) => console.log(`${error} did not connect`));
+}).catch((erorr) => console.log(`${erorr} did not connect`));
  
 
 

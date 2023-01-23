7cfe6098f87144210b2f22b50dbed92fd6a9835c
MEdaHad <medahad16@gmail.com>
2023-01-23 21:50:27 +0100

    routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,3 +36,20 @@ export const register = async (req, res) =>{
         res.status(500).json({error: err.message });        
     }
 };
+
+export const login = async (req, res) =>{
+    try {
+        const {email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "User doesnt exist"});
+
+        const isMatch = await bcrypt.compare(password, user.password); 
+        if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials."});
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+
+    } catch (err){
+        res.status(500).json({ error: err.message });
+    }
+}
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 
@@ -36,6 +37,8 @@ const storage = multer.diskStorage({
 const upload = multer ({ storage });
 
 app.post("/auth/register", upload.single("picture"), register);
+
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
